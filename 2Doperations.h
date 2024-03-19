320220d7a965b4b182f7e7cab6bd0b83f9da56be
Zawarudo1001 <78776153+Zawarudo1001@users.noreply.github.com>
2024-03-19 11:10:29 +0300

    Add files via upload

---
+++ 2Doperations.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "2dTypes.h"
+
+float length(Vec2D const& v) { return sqrt(v.x * v.x + v.y * v.y); }
+float dot(Vec2D const& a, Vec2D const& b) { return a.x * b.x + a.y * b.y; }
+Vec2D norm(Vec2D &v) { return v / length(v); }
+float length(Point2d const& p1, Point2d const& p2) { return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)); }
+
+
+Vec3D FindCenRad(Point2d &a, Point2d &b, Point2d &c) {
+	float A = b.x - a.x;
+	float B = b.y - a.y;
+	float C = c.x - a.x;
+	float D = c.y - a.y;
+	float E = A * (a.x + b.x) + B * (a.y + b.y);
+	float F = C * (a.x + c.x) + D * (a.y + c.y);
+	float G = 2 * (A * (c.y - b.y) - B * (c.x - b.x));
+	// Если G = 0, это значит, что через данный набор точек провести окружность нельзя.
+	if (G == 0) return -1;
+
+	float Cx = (D * E - B * F) / G;
+	float Cy = (A * F - C * E) / G;
+	float R = sqrt(powf(a.x - Cx, 2) + powf(a.y - Cy, 2));
+	return Vec3D{ Cx, Cy, R };
+}
+
+
+Vec2D FindNorm(Point2d &A, Point2d &B, Point2d &C) {
+	Vec2D AB = Vec2D{ A, B };
+	AB = norm(AB);
+
+	Vec2D AC = Vec2D{ A, C };
+	float t = dot(AB, AC);			//получаем длину проекции
+
+	float x = A.x + AB.x * t;
+	float y = A.y + AB.y * t;
+	Vec2D n = Vec2D{ C.x - x, C.y - y };		//Получили точку проекции на отрезок, теперь получим вектор нормали к нему и нормализуем его
+	return norm(n);
+}
+
+
+Vec2D NormCircle(Point2d P, Circle circ) {
+	//предполагаем, что тока лежит на окружности. В таком случае вектор нормали будет равен нормализованному вектору между центром и точкой на ней.
+	Vec2D n = Vec2D{ P.x - circ.c.x, P.y - circ.c.y };
+	return norm(n);
+	//возвращаем вектор нормали в точке
+}
---
+++ ConsoleApplication21.cpp
@@ -0,0 +1,94 @@
+﻿#include "2dTypes.h"
+#include "2Doperations.h"
+#include <iostream>
+#include <vector>
+#include <map>
+
+
+#define FLT_EPSILON 1.19e-07 
+#define MAX_FLT_VAL 3.402823466e+38
+
+using namespace std;
+
+
+Vec2D getGeneralNormVect(vector<Point2d> &pointData) {
+	Vec2D n = 0;
+	int t = pointData.size();
+	if (t > 0) {
+		Point2d P_0 = pointData[0];
+		Point2d P_N = pointData[--t];
+		for (int i = 1; i < t; i++) {
+			n = n + FindNorm(P_0, P_N, pointData[i]);
+		}
+		if ((length(n) - 0) < FLT_EPSILON) return 0;		//если усредненный вектор нормали в конечном итоге оказался близок к нулю, то центр окружности найти будет невозможно
+		else return norm(n);
+	}
+	else return 0;
+}
+
+
+vector<Circle> CalcCentRad(Vec2D &n, vector<Point2d> &pointData) {
+	vector<Circle> res;
+	int t = pointData.size();
+
+	if (t > 0) {
+		Point2d P_0 = pointData[0];
+		Point2d P_N = pointData[--t];
+		for (int i = 1; i < t; i++) {
+			Vec3D data = FindCenRad(P_0, pointData[i], P_N);
+			Circle temp{ Point2d{data.x, data.y}, data.w };
+			Vec2D compN = NormCircle(pointData[i], temp);
+			float m = dot(compN, n);
+			if (m > 0) {			//на данном этапе отсекаем проблемные и шумные точки. В конечном итоге работа продолжится только с наиболее подходящими точками
+				res.push_back(Circle{ temp.c, data.w });		//вносим найденный радиус и координаты центра окружности
+			}
+		}
+	}
+	return res;		//в итоговом списке получим список центров окружностей и их радиусов, которые попадают под необходимые условия
+}
+
+
+Circle GetOptimalParameters(vector<Circle> &points, vector<Point2d> &pointData) {
+	int s = points.size();
+	int p = pointData.size();
+	float temp = 0;
+	float dist = MAX_FLT_VAL;
+	int index;
+
+	for (int i = 0; i < s; i++) {			//в этом цикле проходимся по всем найденным центрам окружностей
+		temp = 0;
+		for (int j = 0; j < p; j++) {			//в этом цикле проходимся по каждой точке на дуге
+			temp += length(points[i].c, pointData[j]) - points[i].r;		//для каждой точки находим расстояние до окружности и суммируем отрицательные и положительные расстояния, получая среднее
+		}
+		temp = fabs(temp);
+		if (temp < dist) {
+			dist = temp;				//стремимся получить расстояние наиболее близкое к 0, находим минимальное расстояние
+			index = i;
+		}
+	}
+	return points[index];
+}
+
+
+int main() {
+
+	vector<Point2d> pointData;
+	pointData.push_back({ 1,1 });
+	pointData.push_back({ 1.5f,2 });
+	pointData.push_back({ 2.5,2.5 });
+	pointData.push_back({ 3.5,2.5 });
+	pointData.push_back({ 4.5,2 });
+	pointData.push_back({ 5,1 });
+
+	/*тут храним набор из точек, с которыми будет производиться работа
+	будем считать, что первый и последний член списка является концевыми точками для дуги P0, Pn
+	*/
+
+	Vec2D n = getGeneralNormVect(pointData);		//получили по точкам вектор нормали, который определяет изгиб дуги окружности
+	vector<Circle> RadMap = CalcCentRad(n, pointData);		//посчитали возможные центры окружностей и радиусы, отфильтровали
+
+	Circle Ans = GetOptimalParameters(RadMap, pointData);
+
+	cout << Ans.c.x << " " << Ans.c.y << " " << Ans.r << endl;	// итоговая дуга окружности по выбранным параметрам будет включать концевые точки и оптимальным образом объединит точки маршрута
+	return 0;
+}
